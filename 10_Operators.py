4ceef1bc17db557af39687adf50ce65c955b0a6d
Kartik <itzkartikeds3@gmail.com>
2023-10-06 21:08:00 +0530

    Operators & it's types

---
+++ 10_Operators.py
@@ -0,0 +1,35 @@
+# OPERATORS :
+# 1. Arithmetic Operators :
+#     +
+#     -
+#     *
+#     /
+# 2. Assignment Operators :
+#     =
+#     Shortcuts
+#     -> x = x + a <=> x += a
+#     -> x = x - a <=> x -= a
+#     -> x = x * a <=> x *= a
+#     -> x = x / a <=> x /= a
+
+#     Assignment can be done in 1 line
+#     a,b = 5,6
+# 3. Relational Operators : (Returns True or False)
+#     > (greater than)
+#     < (less than)
+#     == (equal to)
+#     <= (greater than or equal to)
+#     >= (less than or equal to)
+#     != (Not equal to)
+# 4. Logical Operators :
+#     and
+#     or
+#     not 
+# 5. Unary Operator :
+#     n = 7
+#     n = -n # This makes n = -7
+
+x = True
+print("Before using not operator", x)
+x= not x
+print("After using not operator",x)

5629966facb7042af4609c0b74875c9f6925ac4a
Kartik <itzkartikeds3@gmail.com>
2023-10-08 01:56:24 +0530

    continue

---
+++ 25_Continue.py
@@ -0,0 +1,9 @@
+# continue -> Print numbers from 1-20 but don't print numbers which are divisible by 3 or 5
+for i in range(1,101):
+    if(i % 3 == 0 or i % 5 == 0):
+        continue # Skips remaining code inside loop for this iteration & moves onto next iteration
+        # It will not jump out of the loop 
+    print(i)
+
+
+print("Bye!")

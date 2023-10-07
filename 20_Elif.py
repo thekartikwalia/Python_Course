a11e2d8ca1b0914dac0b7e687e5c869d10217781
Kartik <itzkartikeds3@gmail.com>
2023-10-07 23:43:52 +0530

    elif

---
+++ 20_Elif.py
@@ -0,0 +1,24 @@
+x = 2
+if(x==1):
+    print("One")
+if(x==2):
+    print("Two")
+if(x==3):
+    print("Three")
+if(x==4):
+    print("Four")
+
+# BETTER REPLACEMENT OF ABOVE CODE
+# elif stands for else-if 
+# elif tells us to check this statement if statement under above if isn't True
+x = 23
+if (x == 1):
+    print("One")
+elif (x == 2):
+    print("Two")
+elif (x == 3):
+    print("Three") 
+elif (x == 4):
+    print("Four")
+else:
+    print("Wrong Input")

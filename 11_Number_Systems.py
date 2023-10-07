a5ff5782906c71406cc93f96624600801bedb465
Kartik <itzkartikeds3@gmail.com>
2023-10-07 09:41:59 +0530

    Number Systems

---
+++ 11_Number_Systems.py
@@ -0,0 +1,21 @@
+# NUMBER SYSTEMS :
+# 1. Binary :
+#     base 2 -> 0-1 (2 digits)
+# 2. Decimal :
+#     base 10 -> 0-9 (10 digits)
+# 3. Octal :
+#     base 8 -> 0-7 (8 digits)`
+# 4. HexaDecimal :
+#     base 16 -> 0-9 a-f (16 digits)
+
+# bin() -> used to convert a number from decimal system to binary system 
+print(bin(25)) # -> gives 0b11001, here 0b represents that number is in binary format 
+
+# oct() -> used to convert a number from decimal system to octal system 
+print(oct(25)) # -> gives 0o31, here 0o represents that number is in octal format 
+
+# hex() -> used to convert a number from decimal system to hexadecimal system 
+print(hex(25)) # -> gives 0x19, here 0x represents that number is in hexadecimal format 
+print(0xf) # -> gives 15 , example to show that reverse can also work
+
+

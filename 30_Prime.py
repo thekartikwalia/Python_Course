ab938bab4c37c1a8b72ebcb5525e3f24c7f9673f
Kartik <itzkartikeds3@gmail.com>
2023-10-08 10:30:19 +0530

    Prime Number

---
+++ 30_Prime.py
@@ -0,0 +1,10 @@
+num = 7
+for i in range(2,num):
+    if(num % i == 0):
+        print("Not Prime")
+        break
+else:
+    print("Prime")
+
+
+# More efficient way of doing this would be by checking divisibility only till half rather than till n-1

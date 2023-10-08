f0d6df95780b681e935ffeeeb459059c64b4aec1
Kartik <itzkartikeds3@gmail.com>
2023-10-08 12:10:25 +0530

    Numpy basics

---
+++ 35_Numpy.py
@@ -0,0 +1,7 @@
+from numpy import *
+arr = array([1, 2, 3, 4, 5, 6]) # In numpy, you don't need to mention the datatype
+print(arr)
+
+# Mentioning the data type in numpy is a choice, not compulsary
+arr = array([1, 2, 3, 4, 5, 6], int)
+print(arr)

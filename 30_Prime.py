a45de07a65af7c9a2835b115977e206807d00445
Kartik <itzkartikeds3@gmail.com>
2023-10-08 10:33:08 +0530

    Prime Number efficieny increased

--- 30_Prime.py
+++ 30_Prime.py
@@ -8,3 +8,10 @@ else:
 
 
 # More efficient way of doing this would be by checking divisibility only till half rather than till n-1
+num = 11
+for i in range(2, num//2):
+    if (num % i == 0):
+        print("Not Prime")
+        break
+else:
+    print("Prime")

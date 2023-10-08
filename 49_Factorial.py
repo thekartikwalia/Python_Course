ddd86ec6df0a9bb8a66d327af4e9f92e60961b34
Kartik <itzkartikeds3@gmail.com>
2023-10-08 23:50:41 +0530

    Factorial

---
+++ 49_Factorial.py
@@ -0,0 +1,13 @@
+# Factorial of n = n! = 1x2x3x4 ...(till n)
+
+def fact(n):
+    f = 1
+    for i in range(1,n+1):
+        f = f * i
+        i+=1
+    return f
+
+
+x = 4
+result = fact(x)
+print(result)
